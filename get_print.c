052ba999f9b56b5e0ce7ec39b5d9128a31080b19
Tonycrz <tonybaidoo0@gmail.com>
2021-10-04 17:22:11 +0000

    updatE

--- get_print.c
+++ get_print.c
@@ -16,17 +16,7 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
-		{'d', print_int},
-/**		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal},
-		{'R', print_rot13},
-		{'r', print_rev},
-		{'S', print_bigS},
-		{'p', print_address},
-		{'%', print_percent} */
+		{'d', print_int}
 		};
 	int flags = 14;
 
